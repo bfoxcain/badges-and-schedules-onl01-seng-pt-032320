60a882a7e44156a9f9fe15932f10a06d499a92d6
Beverly Appleton <bfcain19@gmail.com>
2020-03-30 17:08:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,8 @@
 # Write your code here.
 #attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace', "Linus", "Matz"]
 
-def badge_maker(attendees)
-   puts "Hello, my name is #{attendees}."
+def badge_maker(name)
+   puts "Hello, my name is #{name}."
 
 end
 
