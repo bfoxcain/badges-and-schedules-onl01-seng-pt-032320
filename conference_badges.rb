339cacedfeea1336a2793a50ee5d1d03461ff699
Beverly Appleton <bfcain19@gmail.com>
2020-03-30 20:03:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,6 +7,11 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
+  nuarray = []
+  array.each do |name|
+    nuarray.push("Hello, my name is#{name}.")
+  end
+  return nuarray
 
 end
 
