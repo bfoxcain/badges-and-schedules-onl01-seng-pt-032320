7b9cee4e2ac8cfaa010546ae75656aebdbb0aaee
Beverly Appleton <bfcain19@gmail.com>
2020-03-30 16:39:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,15 @@ def badge_maker(name)
    puts "Hello, my name is #{name}."
 
 end
+
+def batch_badge_creator(attendees)
+
+end
+
+def assign_rooms(attendees)
+
+end
+
+def printer(attendees)
+
+end
